9326ccfe582c85bcbefe2145d9255cd7bbc8bd92
leonardo <leonardo>
2025-03-17 09:05:00 -0300

    ""

--- src/App.js
+++ src/App.js
@@ -86,6 +86,26 @@ function App() {
       <header>
         <strong>React Nutri</strong>
       </header>
+
+      {nutri.map((item)=> {
+        return(
+          <article key={item.id} className="post">
+            <strong className="titulo">
+              {item.titulo}  
+            </strong>  
+            <img
+              src={item.capa}
+              alt={item.titulo}
+              className="capa"
+            />
+            <p className="subtitulo">
+              {item.subtitulo}
+            </p>
+
+            <a className="botao">Acessar</a>
+          </article>
+        )
+      })}
     </div>
   )
   
