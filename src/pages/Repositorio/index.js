61c43f0d9c156a2fe46bc84e47060cd1a366495c
leonardo <leonardo>
2025-03-19 12:01:15 -0300

    eslint-disable-next-line

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Container, Owner, Loading, BackButton,
-  IssuesList
+  IssuesList, PageActions
  } from './styles';
 import { FaArrowLeft } from 'react-icons/fa';
 import api from '../../services/api';
@@ -11,6 +11,7 @@ export default function Repositorio() {
   const [repo, setRepo] = useState({});
   const [issues, setIssues] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     async function load() {
@@ -38,6 +39,29 @@ export default function Repositorio() {
     
     load();
   }, [repositorio]); // Make sure to re-run the effect when `repositorio` changes
+
+  useEffect(() => {
+    
+    async function loadIssue (){
+      const nomeRepo = decodeURIComponent(repositorio);
+
+      const response = await api.get(`/repos/${nomeRepo}/issues`, {
+        params:{
+          state: 'open',
+          page,
+          per_page: 5,
+        }
+      });
+
+      setIssues(response.data);
+
+    };
+  }, [repositorio, page]);
+
+
+  function handlePage (action){
+    setPage(action === 'back' ? page - 1 : page + 1)
+  }
 
   if (loading) {
     return (
@@ -67,15 +91,29 @@ export default function Repositorio() {
             <img src={issue.user.avatar_url} alt={issue.user.login} />
             <div>
               <strong>
-              <a href={issue.html_url} target="_blank" rel="noopener noreferrer">
-                {issue.title}
-              </a>
+                <a href={issue.html_url} target="_blank" rel="noopener noreferrer">
+                {issue.title}</a>
               </strong>
+                {issue.labels.map(label => (
+                  <span key={String(label.id)}>{label.id}</span>
+              ))}
             </div>
             
           </li>
         ))}
       </IssuesList>
+      <PageActions>
+        <button type='button' 
+        onClick={() => handlePage('back') }
+        disabled={page < 2}
+        > 
+          Voltar 
+        </button>
+
+        <button type='button' onClick={() => handlePage('next')}> 
+          Proxima 
+        </button>
+      </PageActions>
     </Container>
   );
 }
--- src/pages/Repositorio/styles.js
+++ src/pages/Repositorio/styles.js
@@ -2,54 +2,128 @@ import styled from "styled-components";
 import { Link } from 'react-router-dom';
 
 export const Loading = styled.div`
-    color: #fff;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
+color: #fff;
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100vh;
 `;
 
 export const Container = styled.div`
-    max-width: 400px;
-    background-color: #fff;
-    border-radius: 4px;
-    box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
-    padding: 30px;
-    margin: 80px auto;
+max-width: 500px;
+background-color: #fff;
+border-radius: 4px;
+box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+padding: 30px;
+margin: 80px auto;
 `;
 
 export const BackButton = styled(Link)`
-    border: 0;
-    outline: 0;
-    background-color:  transparent;
+border: 0;
+outline: 0;
+background-color:  transparent;
 `;
 
 export const Owner = styled.header`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+display: flex;
+flex-direction: column;
+align-items: center;
 
-    img{
-        width: 150px;
-        border-radius: 20%;
-        margin: 20px 0;
-    }
+img{
+    width: 150px;
+    border-radius: 20%;
+    margin: 20px 0;
+}
 
-    h1{
-        font-size: 30px;
-        color: #0D2636;
-    }
+h1{
+    font-size: 30px;
+    color: #0D2636;
+}
 
-    p{
-        margin-top: 5px;
-        font-size: 14px;
-        color: #000;
-        text-align: center;
-        line-height: 1.4;
-        max-width: 400px;
-    }    
+p{
+    margin-top: 5px;
+    font-size: 14px;
+    color: #000;
+    text-align: center;
+    line-height: 1.4;
+    max-width: 400px;
+}    
 `;
 
 export const IssuesList = styled.ul`
+margin-top: 30px;
+padding-top: 30px;
+border-top: 1px solid #eee;
+list-style: none;
+
+li{
+    display: flex;
+    padding:  15px 10px;
     
+    & + li{
+        margin-top: 12px;
+    }
+}
+
+img{
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+    border: 2px solid #0D2636;
+}
+
+div{
+    flex: 1;
+    margin-left: 12px;
+    
+    p{
+        margin-top: 10px;
+        font-size: 12px;
+        color: #000;
+    }
+    
+}
+strong{
+    font-size: 15px;
+    
+    a{
+        text-decoration: none;
+        color: #222;
+        transform: 0.3s;
+        
+        &:hover{
+            color: #0071db;
+        }
+    }
+    
+    span{
+        background-color: #222;
+        color: #fff;
+        border-radius: 4px;
+        font-size: 12px;
+        font-weight: 600;
+        padding: 5px 7px;
+        margin-left: 10px;
+    }
+}
+`;
+
+export const PageActions = styled.div`
+    display: flex;
+    align-items: center ;
+    justify-content: space-between;
+
+    button{
+        outline: 0;
+        border: 0;
+        background-color: #222;
+        color: #fff;
+        padding: 5px 10px;
+        border-radius: 4px;
+
+        &:disabled{
+            cursor: not-allowed;
+            opacity: 0.5;
+        }
+    }
 `;
