2865e11a37267cfd719eebf25d2cd16422532e95
Leo Gomes <106993889+leo-gomes-dev@users.noreply.github.com>
2026-08-17 18:32:23 -0300

    remove pack lock

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,10 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { Container, Owner, Loading, BackButton,
-  IssuesList, PageActions, FilterList
- } from './styles';
-import { FaArrowLeft } from 'react-icons/fa';
-import api from '../../services/api';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  Container,
+  Owner,
+  Loading,
+  BackButton,
+  IssuesList,
+  PageActions,
+  FilterList,
+} from "./styles";
+import { FaArrowLeft } from "react-icons/fa";
+import api from "../../services/api";
 
 export default function Repositorio() {
   const { repositorio } = useParams(); // Use this only once
@@ -13,14 +19,14 @@ export default function Repositorio() {
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(1);
   const [filters, setFilters] = useState([
-    { state: 'all', label: 'Todas', active: true },
-    { state: 'open', label: 'Abertas', active: false },
-    { state: 'closed', label: 'Fechadas', active: false },
+    { state: "all", label: "Todas", active: true },
+    { state: "open", label: "Abertas", active: false },
+    { state: "closed", label: "Fechadas", active: false },
   ]);
 
   const [filterIndex, setFilterIndex] = useState(0);
 
-   useEffect(() => {
+  useEffect(() => {
     async function load() {
       const nomeRepo = decodeURIComponent(repositorio); // Decodifica o nome do repositório
 
@@ -46,13 +52,10 @@ export default function Repositorio() {
     }
 
     load();
-    
-    // CORRIGIDO: Array completo incluindo todas as dependências lidas pelo hook
-  }, [filters, page, repositorio]); 
-
+  }, [filters, page, repositorio]);
 
   function handlePage(action) {
-    setPage(action === 'back' ? page - 1 : page + 1);
+    setPage(action === "back" ? page - 1 : page + 1);
   }
 
   function handleFilter(index) {
@@ -76,13 +79,10 @@ export default function Repositorio() {
   return (
     <Container>
       <BackButton to="/">
-        <FaArrowLeft color='#000' size={30} />
+        <FaArrowLeft color="#000" size={30} />
       </BackButton>
       <Owner>
-        <img 
-          src={repo.owner.avatar_url} 
-          alt={repo.owner.login} 
-        />
+        <img src={repo.owner.avatar_url} alt={repo.owner.login} />
         <h1>{repo.name}</h1>
         <p>{repo.description}</p>
       </Owner>
@@ -90,27 +90,31 @@ export default function Repositorio() {
       <FilterList active={filterIndex}>
         {filters.map((filter, index) => (
           <button
-            type='button'
+            type="button"
             key={filter.label}
             onClick={() => handleFilter(index)}
-          > 
+          >
             {filter.label}
           </button>
         ))}
       </FilterList>
 
       <IssuesList>
-        {issues.map(issue => (
+        {issues.map((issue) => (
           <li key={String(issue.id)}>
             <img src={issue.user.avatar_url} alt={issue.user.login} />
             <div>
               <strong>
-                <a href={issue.html_url} target="_blank" rel="noopener noreferrer">
+                <a
+                  href={issue.html_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   {issue.title}
                 </a>
               </strong>
-              {issue.labels.map(label => (
-                <span key={String(label.id)}>{label.name}</span>  
+              {issue.labels.map((label) => (
+                <span key={String(label.id)}>{label.name}</span>
               ))}
             </div>
           </li>
@@ -118,18 +122,15 @@ export default function Repositorio() {
       </IssuesList>
 
       <PageActions>
-        <button 
-          type='button' 
-          onClick={() => handlePage('back')} 
+        <button
+          type="button"
+          onClick={() => handlePage("back")}
           disabled={page < 2}
         >
           Voltar
         </button>
 
-        <button 
-          type='button' 
-          onClick={() => handlePage('next')}
-        >
+        <button type="button" onClick={() => handlePage("next")}>
           Próxima
         </button>
       </PageActions>
